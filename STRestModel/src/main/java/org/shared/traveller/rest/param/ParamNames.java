5159fc7cd6c294872f2137e96b367373516385c0
AlexzanderIvanov <alexzander.t.ivanov@gmail.com>
2013-12-01 16:35:04 +0200

    * Added common query param names class.

---
+++ STRestModel/src/main/java/org/shared/traveller/rest/param/ParamNames.java
@@ -0,0 +1,9 @@
+package org.shared.traveller.rest.param;
+
+public class ParamNames
+{
+	private static final String = "from";
+	private static final String = "to";
+	private static final String = "date";
+	private static final String = "sortOrder";
+}
