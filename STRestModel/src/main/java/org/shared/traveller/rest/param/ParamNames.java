59ffa3999f9ae0574ddcb3325c0ca6c873e63a6e
AlexzanderIvanov <alexzander.t.ivanov@gmail.com>
2013-12-01 16:40:38 +0200

    * Added start and count query param names.

--- STRestModel/src/main/java/org/shared/traveller/rest/param/ParamNames.java
+++ STRestModel/src/main/java/org/shared/traveller/rest/param/ParamNames.java
@@ -2,6 +2,8 @@ package org.shared.traveller.rest.param;
 
 public class ParamNames
 {
+	public static final String START = "start";
+	public static final String COUNT = "count";
 	public static final String FROM = "from";
 	public static final String TO = "to";
 	public static final String DATE = "date";
